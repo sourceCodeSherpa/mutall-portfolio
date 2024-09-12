6b39245c74ee8896b20f87e6459d5dfe5f96c0a8
wairua <wairua.joshua@gmail.com>
2024-09-12 18:31:56 +0300

    sql to list client names, room, and meter number

--- v/code/client_room.sql
+++ v/code/client_room.sql
@@ -1,14 +1,14 @@
 /*
-        +-------------|----------------|
-        |   name       |      uid      |
-        |------__----------------------+
-        |  john doe    |    1          |
-        |------------------------------|
+        +-------------|-----------------------|
+        |   client name    |     room no.     |
+        |-------------------------------------+
+        |  john doe        |       1          |
+        |-------------------------------------|
 */
 -- list clients names and the room they are assigned to
 select
 -- columns to display
-    client.`name` as "client name", room.uid as "room number"
+    client.`name` as "client name", room.uid as "room no."
 from
 -- data source
     client
