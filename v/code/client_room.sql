cf54e5d6cf45b02a482f6d8d8e23227f9e333db3
wairua <wairua.joshua@gmail.com>
2024-09-12 17:25:21 +0300

    sql to list client name and their room ID

---
+++ v/code/client_room.sql
@@ -0,0 +1,16 @@
+/*
+        +-------------|----------------|
+        |   name       |      uid      |
+        |------__----------------------+
+        |  john doe    |    1          |
+        |------------------------------|
+*/
+-- list clients names and the room they are assigned to
+select
+-- columns to display
+    client.`name` as "client name", room.uid as "room number"
+from
+-- data source
+    client
+INNER JOIN agreement ON client.client = agreement.client
+INNER JOIN room ON agreement.room = room.uid
